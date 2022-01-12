f09e028727c77d70671207429e1b4ad4f23fc0cb
mateusznieciecki <73481114+mateusznieciecki@users.noreply.github.com>
2022-01-12 10:23:07 +0100

    Delete JavaSpringJSP/src directory

--- JavaSpringJSP/src/main/java/com/example/Szpital/repositories/IWpisyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Szpital.repositories;
-
-import com.example.Szpital.entities.Wpisy;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IWpisyRepository extends JpaRepository<Wpisy, Integer> {
-}
